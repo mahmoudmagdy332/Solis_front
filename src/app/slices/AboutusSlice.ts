001914805dfb148c4a315096e09eeb44410f630a
mahmoud magdy <mahmoudmagdy99925332@gmail.com>
2024-10-02 17:55:08 +0300

    sdfj

--- src/app/slices/AboutusSlice.ts
+++ src/app/slices/AboutusSlice.ts
@@ -1,27 +1,27 @@
-// languageSlice.js
-import { createSlice } from "@reduxjs/toolkit";
-import { useSelector } from "react-redux";
-import { RootState } from "../store";
-import { AboutSliceType } from "../utils/types/types";
+// // languageSlice.js
+// import { createSlice } from "@reduxjs/toolkit";
+// import { useSelector } from "react-redux";
+// import { RootState } from "../store";
+// import { AboutSliceType } from "../utils/types/types";
 
-const initialState: AboutSliceType = {
-  About: null,
-  Benefits: null,
-};
+// const initialState: AboutSliceType = {
+//   About: null,
+//   Benefits: null,
+// };
 
-const aboutUsSlice = createSlice({
-  name: "AboutUs",
-  initialState,
-  reducers: {
-    setAboutUs: (state, action) => {
-      state.About = action.payload.aboutUs;
-      state.Benefits = action.payload.benefits;
-    },
-  },
-});
+// const aboutUsSlice = createSlice({
+//   name: "AboutUs",
+//   initialState,
+//   reducers: {
+//     setAboutUs: (state, action) => {
+//       state.About = action.payload.aboutUs;
+//       state.Benefits = action.payload.benefits;
+//     },
+//   },
+// });
 
-export const { setAboutUs } = aboutUsSlice.actions;
+// export const { setAboutUs } = aboutUsSlice.actions;
 
-export default aboutUsSlice.reducer;
+// export default aboutUsSlice.reducer;
 
-export const useAboutUsSliceSelector = useSelector.withTypes<RootState>();
+// export const useAboutUsSliceSelector = useSelector.withTypes<RootState>();
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -16,13 +16,43 @@ const Footer = () => {
                  <Link to="/" className="text-white">residential</Link>
              </div>
              <div className="flex flex-col gap-2    ">
-                 <p  className="font-bold text-white">Products</p>
+                 <p  className="font-bold text-white">Applications</p>
+                 <Link to="/" className="text-gray-300 text-md">Gas Stations & C-Stores</Link>
+                 <Link to="/" className="text-gray-300 text-md">Cities & Towns</Link>
+                 <Link to="/" className="text-gray-300 text-md">Commercial</Link>
+                 <Link to="/" className="text-gray-300 text-md">Transportation</Link>
+                 <Link to="/" className="text-gray-300 text-md">Schools & Universities</Link>
+                 <Link to="/" className="text-gray-300 text-md">Auto Dealerships</Link>
+                 <Link to="/" className="text-gray-300 text-md">Commercial</Link>
+                 <Link to="/" className="text-gray-300 text-md">Transportation</Link>
+                 <Link to="/" className="text-gray-300 text-md">Schools & Universities</Link>
+                 <Link to="/" className="text-gray-300 text-md">Auto Dealerships</Link>
+
+             </div>
+             <div className="flex flex-col gap-2    ">
+                 <p  className="font-bold text-white">Commercial Support</p>
                  <Link to="/" className="text-gray-300 text-md">indoor</Link>
+                 <Link to="/" className="text-gray-300 text-md">Request Quote</Link>
+                 <Link to="/" className="text-gray-300 text-md">Warranty</Link>
+                 <Link to="/" className="text-gray-300 text-md">Warranty</Link>
+                 <Link to="/" className="text-gray-300 text-md">Submit Claim</Link>
+             </div>
+             <div className="flex flex-col gap-2    ">
+                 <p  className="font-bold text-white">Partners & Suppliers</p>
+                 <Link to="/" className="text-gray-300 text-md">Contractor Information</Link>
+                 <Link to="/" className="text-gray-300 text-md">Supplier Resources</Link>
+                 <Link to="/" className="text-gray-300 text-md">Supplier Information Form</Link>
+
                  <Link to="/" className="text-gray-300 text-md">residential</Link>
-                 <Link to="/" className="text-gray-300 text-md">indoor</Link>
-                 <Link to="/" className="text-gray-300 text-md">residential</Link>
-                 <Link to="/" className="text-gray-300 text-md">indoor</Link>
-                 <Link to="/" className="text-gray-300 text-md">residential</Link>
+             </div>
+             <div className="flex flex-col gap-2    ">
+                 <p  className="font-bold text-white">Contact</p>
+                 <Link to="/" className="text-gray-300 text-md">About</Link>
+                 <Link to="/" className="text-gray-300 text-md">Careers</Link>
+                 <Link to="/" className="text-gray-300 text-md">News & Releases</Link>
+                 <Link to="/" className="text-gray-300 text-md">Legal</Link>
+                 <Link to="/" className="text-gray-300 text-md">Licensing</Link>
+                 <Link to="/" className="text-gray-300 text-md">Slavery and Human Trafficking Statement</Link>
              </div>
            </div>
            <div className="flex justify-center mt-6 text-sm text-gray-300">
