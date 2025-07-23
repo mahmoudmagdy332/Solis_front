cd475e4109c50c9c2f2f4a0911d47b044d739cac
saied40 <noah12.gm@gmail.com>
2025-07-23 19:57:35 +0300

    edits

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -20,23 +20,23 @@ type IntroTranslation = {
   title: string;
   created_at: string;
   updated_at: string;
-}
+};
 
 export default function Hero({ intro }: { intro: Intro[] }) {
   const { lang, translations } = useLanguageSelector((state) => state.LanguageReducer);
   const { main_categories } = useSettingSliceSelector((state) => state.settingReducer);
 
   return (
-    <div className="">
-      <Swiper loop autoplay={{ delay: 3000, pauseOnMouseEnter: true, disableOnInteraction: false }} modules={[Autoplay]}>
+    <div className="max-h-[80vh] lg:mt-14 mt-20">
+      <Swiper loop autoplay={{ delay: 3000, pauseOnMouseEnter: true, disableOnInteraction: false }} modules={[Autoplay]} className="h-full">
         {intro.map((item) => {
           const t = item.translations.find((t) => t.locale === lang);
           return (
-            <SwiperSlide key={item.id} className="max-h-[80vh] relative">
-              <img src={item.image} alt={t?.title} className="w-full h-full object-cover" />
+            <SwiperSlide key={item.id} className="h-full relative max-h-[80vh] min-h-[470px]">
+              <img src={item.image} alt={t?.title} className="min-w-full min-h-full max-w-[unset] object-cover" />
               <div className="z-10 absolute top-0 left-0 size-full bg-black/50" />
-              <div className="mx-12 w-1/2 absolute top-1/2 left-0 -translate-y-1/2 z-50">
-                <p dangerouslySetInnerHTML={{ __html: t?.title || "" }} className="text-white text-3xl text-wrap break-words" />
+              <div className="mx-4 lg:mx-12 w-full lg:w-1/2 absolute top-1/2 left-0 -translate-y-1/2 z-50">
+                <p dangerouslySetInnerHTML={{ __html: t?.title || "" }} className="text-white text-3xl text-wrap //break-words truncate line-clamp-2 max-w-[90%]" />
                 {main_categories[0] && (
                   <Link
                     to={`/main-category/${main_categories[0].id}/${main_categories[0].name}`}
